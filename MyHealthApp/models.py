186f3239ec2512b37c962a5792bdf9dff8346d20
Abhik <abhikroy10@gmail.com>
2017-06-07 16:04:45 +0530

    abhik

--- MyHealthApp/models.py
+++ MyHealthApp/models.py
@@ -3,16 +3,20 @@ from django.db import models
 class Symptom(models.Model):
     symptom_name = models.CharField(max_length=1000)
     body_parts = models.ManyToManyField(Bodypart)
+    medicines = models.ManyToManyField(Medicine)
     # Disease
-
+    def __str__(self):
+        return self.symptom_name
 
 class Insurance(models.Model):
     insurance_plan= models.CharField(max_length=500)
     expiry date = models.DateField
     start_date= models.DateField
     # User_id
-    user = models.ManyToManyField(User)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.insurance_plan
 
 class Bodypart(models.Model):
     bodypart= {
@@ -20,3 +24,8 @@ class Bodypart(models.Model):
                 'middle':'neck, chest or stomach'
                 'down': 'thighs and legs'
             }
+     medicines = models.ManyToManyField(Medicine)
+
+
+def __str__(self):
+    return self.bodypart

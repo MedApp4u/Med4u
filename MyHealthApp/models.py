8789e1399295cf3377433cdc3eeef1ff2a0e7873
Abhik <abhikroy10@gmail.com>
2017-06-07 15:53:07 +0530

    Symptom, insurance and bodypart

--- MyHealthApp/models.py
+++ MyHealthApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Symptom(models.Model):
+    symptom_name = models.CharField(max_length=1000)
+    body_parts = models.ManyToManyField(Bodypart)
+    # Disease
+
+
+class Insurance(models.Model):
+    insurance_plan= models.CharField(max_length=500)
+    expiry date = models.DateField
+    start_date= models.DateField
+    # User_id
+
+
+class Bodypart(models.Model):
+    bodypart= {
+                'head':'head',
+                'middle':'neck, chest or stomach'
+                'down': 'thighs and legs'
+    }

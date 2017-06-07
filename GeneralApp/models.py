494bd7c07ee320ce5625b9ce913f06343d1859bc
varunyeligar <varunyeligar0310@gmail.com>
2017-06-07 15:35:18 +0530

    Added models in GeneralApp

--- GeneralApp/models.py
+++ GeneralApp/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Disease(models.Model):
+    disease_name=models.CharField(max_length=20)
+    disease_id=models.AutoField(primary_key=True)
+    symptom=models.ForeignKey(Symptom, on_delete=models.CASCADE)
+    procedure=models.ForeignKey(Procedure, on_delete=models.CASCADE)
+    medicine=models.ForeignKey(Medicine,on_delete=models.CASCADE)

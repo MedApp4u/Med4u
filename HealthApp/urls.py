a47a69019d80b0f11d4b60c8bb4f6fe7b37cab1c
Shrut Patel <f2015032@goa.bits-pilani.ac.in>
2017-06-07 14:54:34 +0530

    Added three apps and deleted HealthApp

--- HealthApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'HealthApp'
-urlpatterns = [
-    url(r'login/', views.LoginFormView.as_view(), name='login'),
-    url(r'tnc/', views.tnc, name='tnc'),    
-]

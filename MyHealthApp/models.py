f6c1001f37ec9b3d2b51e224959a4db898314eb8
j-sil <jaishil.patel1@gmail.com>
2017-06-07 16:14:00 +0530

    measurment and procedure models added

--- MyHealthApp/models.py
+++ MyHealthApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from GeneralApp.models import User
+
 
 # Create your models here.
+
+class Measurement(models.Model):
+    blood_pressure = models.CharField(max_length=30)
+    blood_sugar = models.CharField(max_length=30)
+    cholesterol = models.CharField(max_length=30)
+    height = models.FloatField()
+    weight = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Procedure(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+    possible_complication = models.CharField(max_length=300)
+    image = models.FileField()
+    video = models.FileField()
+    bodypart = models.ManyToManyField(Bodypart)
+    symptom = models.ManyToManyField(Symptom)
+    medicine = models.ManyToManyField(Medicine)
+
+    def __str__(self):
+        return self.name
+
+
+

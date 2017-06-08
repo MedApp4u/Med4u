d31516be0b65b22c211ba189b90f6f12de7e3c86
Abhik <abhikroy10@gmail.com>
2017-06-08 10:14:21 +0530

    cabhik

--- MyHealthApp/models.py
+++ MyHealthApp/models.py
@@ -110,3 +110,33 @@ FRONTEND look at this for exit change
     [[[https://stackoverflow.com/questions/34841008/django-timefield-format]]]
 """
     reason = models.TextField()
+
+class Symptom(models.Model):
+    symptom_name = models.CharField(max_length=1000)
+    bodyparts = models.ManyToManyField(Bodypart)
+    medicines = models.ManyToManyField(Medicine)
+    # Disease
+    def __str__(self):
+        return self.symptom_name
+
+class Insurance(models.Model):
+    insurance_plan= models.CharField(max_length=500)
+    expiry date = models.DateField
+    start_date= models.DateField
+    # User_id
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.insurance_plan
+
+class Bodypart(models.Model):
+    bodypart= {
+                'head':'head',
+                'middle':'neck, chest or stomach'
+                'down': 'thighs and legs'
+            }
+    medicines = models.ManyToManyField(Medicine)
+
+
+    def __str__(self):
+        return self.bodypart

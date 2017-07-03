05f19b7ff424c7d9ab855bd2e9b536bf17976b80
harry14 <chizoharsh@gmail.com>
2017-07-03 14:53:34 +0530

    Added test alert for my medicines

--- ProfileApp/static/ProfileApp/js/bootstrap.js
+++ ProfileApp/static/ProfileApp/js/bootstrap.js
@@ -1,3 +1,14 @@
+$(".clickable").click(function(){
+    // $("#div1").load("demo_test.txt", function(responseTxt, statusTxt, xhr){
+    //     if(statusTxt == "success")
+    //         alert("External content loaded successfully!");
+    //     if(statusTxt == "error")
+    //         alert("Error: " + xhr.status + ": " + xhr.statusText);
+    // });
+
+    alert("Medicine has already been added to My Medicines!");
+});
+
 function submitForm(){
 	$('#hidden-medicine-form').submit();
 	$('#hidden-doctor-form').submit();

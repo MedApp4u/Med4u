9bfa1a6af0ce6e0092f4ab527c3dc6a4f99c5827
Shrut Patel <f2015032@goa.bits-pilani.ac.in>
2017-07-07 17:17:42 +0530

    Hashed Password in serializer

--- ProfileApp/serializers.py
+++ ProfileApp/serializers.py
@@ -8,12 +8,12 @@ class ProfileSerializer(serializers.ModelSerializer):
     	model = Profile
         fields=('username','password','email')
 
-        def create(self, validated_data):
-        	password = validated_data.pop('password')
-        	instance = self.Meta.model(**validated_data)
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
 
-        	if password is not None:
-        		instance.set_password(password)
-        		instance.save()
-        		return instance
+        if password is not None:
+        	instance.set_password(password)
+        	instance.save()
+        	return instance
 

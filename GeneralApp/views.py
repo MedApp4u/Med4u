cceefa4bf85e926de7f7a3bed4a50a3e8e16f2a1
Garvit Gupta <f2015036@goa.bits-pilani.ac.in>
2017-07-11 00:41:20 +0530

    Changed MyHealthApp views, modified Symptoms etc

--- GeneralApp/views.py
+++ GeneralApp/views.py
@@ -132,18 +132,18 @@ def BodypartSymptomList(request,symptom_part):
         bodypart = Bodypart.objects.get(bodypart=symptom_part)
         return render(request, 'GeneralApp/symptomlist.html', {'symptoms' : part_symptoms, 'part' : symptom_part, 'part_id' : bodypart.id, 'current_user':request.user})
 
-def SymptomDetails(request, symp_id):
-    symptoms_list = Symptom.objects.all()
+def SymptomDetails(request, symptom_part, symp_id):
+    symptoms_list = Symptom.objects.filter(bodypart__bodypart=symptom_part)
+    current_part = Bodypart.objects.get(bodypart=symptom_part)
     current_symptom = Symptom.objects.get(id=symp_id)
-    bodyparts = Bodypart.objects.filter(symptom__id=symp_id)
+    bodyparts = Bodypart.objects.filter(BPsymptom__id=symp_id)
     s = Symptom.objects.get(id=symp_id)
     videos = s.symptom_videos_set.all()
-    diseases = s.disease_set.all()
-
+    diseases = Disease.objects.filter(symptom__id=symp_id)
 
     if request.method == 'GET':
         return render(request, 'GeneralApp/symptom_details.html',
-                      {'symptoms': symptoms_list, 'current_user': request.user,
+                      {'symptoms': symptoms_list, 'current_part':current_part,'current_user': request.user,
                        'current_symptom': current_symptom, 'bodyparts': bodyparts, 
                        'videos': videos, 'diseases':diseases, 'current_user':request.user})
 
